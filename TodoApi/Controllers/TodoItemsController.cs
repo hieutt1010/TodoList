3dc03947a0a14baee1ad75ce7607cef268373acb
HieuTT <trtrghieu@gmail.com>
2023-12-14 21:15:51 +0700

    add search and change api

--- TodoApi/Controllers/TodoItemsController.cs
+++ TodoApi/Controllers/TodoItemsController.cs
@@ -1,9 +1,9 @@
+using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using TodoApi.Interfaces;
 using TodoApi.Models;
 using TodoApi.Models.DTO;
-using TodoApi.Services;
 
 namespace TodoApi.Controllers
 {
@@ -116,6 +116,11 @@ namespace TodoApi.Controllers
                 return NotFound();
             }
             return NoContent();
+        }
+        [HttpGet("search/{searchString}")]
+        public async Task<List<TodoItemDTO>> Search(string searchString)
+        {
+            return await _todolist.SearchTodoItem(searchString);
         }
     }
 }
